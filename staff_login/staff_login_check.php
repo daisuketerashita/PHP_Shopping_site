08b0ea349c5c77c8b24762203e48e564173d91d2
daisuketerashita <ekusiad.tera@icloud.com>
2021-06-12 19:55:23 +0900

    セッション機能の追加

--- staff_login/staff_login_check.php
+++ staff_login/staff_login_check.php
@@ -37,6 +37,10 @@ require_once('../env.php');
             echo 'スタッフコードかパスワードが間違っています';
             echo "<a href='staff_login.html'>戻る</a>";
         }else{
+            session_start();
+            $_SESSION['login'] = 1;
+            $_SESSION['staff_code'] = $staff_code;
+            $_SESSION['staff_name'] = $rec['name'];
             header('Location: staff_top.php');
             exit();
         }
