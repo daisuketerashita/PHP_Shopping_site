81d73b6d0b661dd53086b5158b2c972f1f3239f4
daisuketerashita <ekusiad.tera@icloud.com>
2021-06-13 21:43:45 +0900

    カートの中身一覧ページの作成

---
+++ shop/shop_cartlook.php
@@ -0,0 +1,79 @@
+<?php
+require_once('../env.php');
+session_start();
+session_regenerate_id(true);
+if(isset($_SESSION['member_login'])==false)
+{
+	echo 'ようこそゲスト様　';
+	echo '<a href="member_login.html">会員ログイン</a><br />';
+	echo '<br />';
+}
+else
+{
+	echo 'ようこそ';
+	echo $_SESSION['member_name'];
+	echo '様　';
+	echo '<a href="member_logout.php">ログアウト</a><br />';
+	echo '<br />';
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>ろくまる農園</title>
+</head>
+<body>
+
+<?php
+
+try
+{
+
+$cart = $_SESSION['cart'];
+$max = count($cart);
+
+//データベース接続
+$dbh = new PDO($dsn,$user,$pass);
+$dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+foreach($cart as $key => $val){
+	$sql = "SELECT code,name,price,image FROM mst_product WHERE code=?";
+	$stmt = $dbh->prepare($sql);
+	$data[0] = $val;
+	$stmt->execute($data);
+
+	$rec = $stmt->fetch(PDO::FETCH_ASSOC);
+
+	$pro_name[] = $rec['name'];
+	$pro_price[] = $rec['price'];
+	if($rec['image'] == ''){
+		$pro_image[] = '';
+	}else{
+		$pro_image[] = "<img src='../product/images/".$rec['image']."'>";
+	}
+}
+
+$dbh = null;
+
+
+}catch(Exception $e){
+	echo'ただいま障害により大変ご迷惑をお掛けしております。';
+	exit();
+}
+
+?>
+
+<h3>カートの中身</h3>
+<?php for($i = 0;$i < $max;$i++): ?>
+<p><?php echo $pro_name[$i]; ?>
+<?php echo $pro_image[$i]; ?>
+<?php echo $pro_price[$i]; ?>円</p>
+<?php endfor; ?>
+<form>
+<input type="button" onclick="history.back()" value="戻る">
+</form>
+
+</body>
+</html>
